d5a295c4c4290346a1f41d700bd549412884e506
Vedavrat <vedavrat@shortcastle.com>
2025-03-18 00:47:22 +0530

    fix: update domain flow

--- src/components/auth-components/Login.tsx
+++ src/components/auth-components/Login.tsx
@@ -84,8 +84,7 @@ export const Login = ({ imageSrc, heading, signUpHref }: LoginProps) => {
   useEffect(() => {
     if (typeof window !== 'undefined') {
       // Access the current URL using the window object
-      // form.setValue('domain', window.location.host);
-      form.setValue('domain', 'localhost');
+      form.setValue('domain', window.location.host);
     }
   }, []);
 
