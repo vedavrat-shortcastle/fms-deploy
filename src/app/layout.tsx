2f6a43bbedfc72e71a30980b8fd4543c469e86f4
Vedavrat <vedavrat@shortcastle.com>
2025-03-17 13:57:00 +0530

    feat: integrate login setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
-import Providers from '@/components/Providers';
+import Providers from '@/components/providers';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
