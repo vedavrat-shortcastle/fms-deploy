8bf55fd0051299ed45f9f1a7ddcb4d8fc8515975
kiran myadaram <myadaramsaikiran@gmail.com>
2025-03-20 16:33:24 +0530

    feat(onboard-parent): added flow to onboard parents

--- src/schemas/Parent.schema.ts
+++ src/schemas/Parent.schema.ts
@@ -3,10 +3,10 @@ import { z } from 'zod';
 export const createParentSchema = z.object({
   phoneNumber: z.string().optional(),
   countryCode: z.string().optional(),
-  streetAddress: z.string(),
+  streetAddress: z.string().min(1, { message: 'Street Address is required' }),
   streetAddress2: z.string().optional(),
-  country: z.string(),
-  state: z.string(),
-  city: z.string(),
-  postalCode: z.string(),
+  country: z.string().min(1, { message: 'Country is required' }),
+  state: z.string().min(1, { message: 'State is required' }),
+  city: z.string().min(1, { message: 'City is required' }),
+  postalCode: z.string().min(1, { message: 'Postal Code is required' }),
 });
--- src/schemas/Player.schema.ts
+++ src/schemas/Player.schema.ts
@@ -34,7 +34,7 @@ export const signupMemberSchema = z.object({
   password: z.string().min(6, 'Password must be at least 6 characters long'),
   firstName: z.string().min(1, 'First name is required'),
   lastName: z.string().min(1, 'Last name is required'),
-  role: z.enum([Role.PLAYER, Role.CLUB_MANAGER]),
+  role: z.enum([Role.PLAYER, Role.CLUB_MANAGER, Role.PARENT]),
 });
 
 export type SignupMemberFormValues = z.infer<typeof signupMemberSchema>;
