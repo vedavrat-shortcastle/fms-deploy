f3e70da688f251416f9a45a7c943e16b16ef0062
Vedavrat <vedavrat@shortcastle.com>
2025-03-21 23:54:46 +0530

    chore: minor import change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import '@/app/globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import Providers from '@/components/Providers';
 
