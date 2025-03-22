09836bfc229bfa60c7349cd124181fd567bb05ec
Vedavrat <vedavrat@shortcastle.com>
2025-03-22 08:59:19 +0530

    fix: add protected route to protected folder

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,12 +1,15 @@
 import Sidebar from '@/components/SideBar';
+import { ProtectedRoute } from '@/hooks/protectedRoute';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <section>
-      <div className="flex h-screen">
-        <Sidebar />
-        <main className="flex-1 h-full overflow-auto">{children}</main>
-      </div>
-    </section>
+    <ProtectedRoute>
+      <section>
+        <div className="flex h-screen">
+          <Sidebar />
+          <main className="flex-1 h-full overflow-auto">{children}</main>
+        </div>
+      </section>
+    </ProtectedRoute>
   );
 }
