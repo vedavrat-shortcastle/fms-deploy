58fd3cccbbb8a33b32321ee8f92e7d009feca565
rspartan-117 <riteksharmachd@gmail.com>
2025-03-28 18:01:32 +0530

    feat: added admin membership route

--- src/app/(protected)/memberships/page.tsx
+++ src/app/(protected)/memberships/page.tsx
@@ -5,7 +5,6 @@ import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { DataTable } from '@/components/usages/DataTable';
 import { adminColumns } from './adminColumns';
-
 import { FileText } from 'lucide-react';
 import {
   Dialog,
@@ -20,11 +19,13 @@ import { usePermission } from '@/hooks/usePermission';
 import Loader from '@/components/Loader';
 import { PERMISSIONS } from '@/config/permissions';
 import { usersColumns } from '@/app/(protected)/memberships/usersColumns';
+import MembersTable from '@/app/(protected)/memberships/MembersTable';
 
 export default function Memberships() {
   const [search, setSearch] = useState('');
   const [formOpen, setFormOpen] = useState(false);
   const canCreatePlan = usePermission(PERMISSIONS.PLAN_CREATE);
+  const [isPlanOpen, setIsPlanOpen] = useState(true);
 
   // Fetch plans using tRPC query
   const { data, isLoading, refetch } = trpc.membership.getPlans.useQuery({
@@ -51,27 +52,46 @@ export default function Memberships() {
   return (
     <div className="flex min-h-svh bg-gray-50">
       <main className="flex-1 flex flex-col p-6">
+        {/* Container Divs */}
+
         <h1 className="text-2xl font-bold flex items-center gap-2">
-          <span className="text-red-600">
+          {/* Heading */}
+          <span className="text-primary">
             <FileText size={20} />
+            {/* Icon */}
           </span>{' '}
           Memberships
         </h1>
         <div className="border-b my-4">
-          <Button variant="link" className="text-red-600 font-semibold">
+          <Button
+            variant="link"
+            className="text-primary font-semibold"
+            onClick={() => setIsPlanOpen(true)}
+          >
             Plans
           </Button>
+          {canCreatePlan && (
+            <Button
+              variant="link"
+              className="text-primary font-semibold"
+              onClick={() => setIsPlanOpen(false)}
+            >
+              Members
+            </Button>
+          )}
         </div>
 
         <div className="flex gap-4 mb-4 items-end">
+          {/* Search */}
           <Input
             placeholder="Search"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
             className="w-64"
           />
-          {canCreatePlan && (
-            <div className="flex  mt-6 items-end w-full h-15 justify-end">
+
+          {canCreatePlan && isPlanOpen && (
+            <div className="flex mt-6 items-end w-full h-15 justify-end">
               {/* Modal Pop-up for Adding usage */}
               <Dialog open={formOpen} onOpenChange={setFormOpen}>
                 <DialogTrigger asChild>
@@ -79,7 +99,7 @@ export default function Memberships() {
                 </DialogTrigger>
 
                 <DialogContent className="max-w-3xl order ">
-                  <DialogHeader className="">
+                  <DialogHeader>
                     <DialogTitle className="text-2xl font-bold">
                       {/* <div className=""> */}
                       <h1 className="text-2xl font-bold">Create Plan</h1>
@@ -99,15 +119,19 @@ export default function Memberships() {
         </div>
 
         {isLoading ? (
-          <div className="flex justify-center w-full h-full items-center ">
+          <div className="flex justify-center items-center min-h-svh">
             <Loader />
           </div>
         ) : (
           <>
-            {canCreatePlan ? (
-              <DataTable columns={adminColumns} data={plans} />
+            {isPlanOpen ? (
+              canCreatePlan ? (
+                <DataTable columns={adminColumns} data={plans} />
+              ) : (
+                <DataTable columns={usersColumns} data={plans} />
+              )
             ) : (
-              <DataTable columns={usersColumns} data={plans} />
+              <MembersTable />
             )}
           </>
         )}
