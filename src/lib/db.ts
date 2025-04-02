516b576914f5fffc561c8803a682868c10f4ffb2
Vedavrat <vedavrat@shortcastle.com>
2025-04-02 19:53:38 +0530

    fix: update max duration for api calls

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,3 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
-export const db = new PrismaClient();
+export const db = new PrismaClient({
+  transactionOptions: {
+    timeout: 10000,
+  },
+});
