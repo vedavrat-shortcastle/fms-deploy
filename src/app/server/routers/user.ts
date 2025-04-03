cafdff769160fee0205b65227bdfb48eb9e83b9c
Vedavrat <vedavrat@shortcastle.com>
2025-04-03 18:36:59 +0530

    fix: add updatelanguage flow back to user router

--- src/app/server/routers/user.ts
+++ src/app/server/routers/user.ts
@@ -1,5 +1,8 @@
 import { protectedProcedure, router } from '@/app/server/trpc';
+import { SupportedLanguages } from '@prisma/client';
 import { TRPCError } from '@trpc/server';
+import i18next from 'i18next';
+import { z } from 'zod';
 
 export const userRouter = router({
   getAdminDetails: protectedProcedure.query(async ({ ctx }) => {
@@ -31,4 +34,35 @@ export const userRouter = router({
 
     return { ...adminDetails, language: adminDetails.profile?.language };
   }),
+  updateLanguage: protectedProcedure
+    .input(
+      z.object({
+        language: z.string(), // Expecting a string language code like 'en', 'fr', etc.
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const userProfile = await ctx.db.userProfile.findUnique({
+        where: {
+          baseUserId: ctx.session.user.id,
+        },
+      });
+
+      if (!userProfile) {
+        throw new Error('User profile not found');
+      }
+
+      const dir = i18next.dir(input.language) === 'rtl' ? true : false;
+
+      const updatedUser = await ctx.db.userProfile.update({
+        where: {
+          baseUserId: ctx.session.user.id,
+        },
+        data: {
+          language: input.language as SupportedLanguages,
+          isRtl: dir,
+        },
+      });
+
+      return updatedUser;
+    }),
 });
